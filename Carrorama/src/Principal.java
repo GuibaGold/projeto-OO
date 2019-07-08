62979562f3d387ef0227c15ae3d39706741e48ca
Guilherme Dourado <guibdourado@gmail.com>
2019-07-08 17:20:25 -0300

    Adicionando UI

--- Carrorama/src/Principal.java
+++ Carrorama/src/Principal.java
@@ -1,63 +1,32 @@
 import proprietario.Veiculo;
-import java.util.Scanner;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		EscreverMenu();
-		Scanner tecladoMenu = new Scanner(System.in);
-		String opcaoMenu = tecladoMenu.nextLine();
-		
+		String[] options = {"1 - Cadastrar Novo Veículo", "2 - Cadastrar Abastecimento", "3 - Cadastrar Despesa", "4 - Relatório Simples", "5 - Relatório de Desempenho", "0 - Sair"};
+		String opcaoMenu = Integer.toString(JOptionPane.showOptionDialog(null, "Selecione uma das opções abaixo:", "Carrorama", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]));
+		Veiculo novoVeiculo = new Veiculo();
+		ArrayList<Veiculo> veiculos = new ArrayList<Veiculo>();
 		do {
-			if(opcaoMenu == "1") {
-				opcaoMenu = "Cadastrar Veículo";
-			} else if(opcaoMenu == "0") {
-				opcaoMenu = "Sair";
-			}
-			MenuEscolha(opcaoMenu);
-		} while(opcaoMenu != null);
-		
+			MenuEscolha(opcaoMenu, novoVeiculo);
+			veiculos.add(novoVeiculo);
+			opcaoMenu = Integer.toString(JOptionPane.showOptionDialog(null, "Selecione uma das opções abaixo:", "Carrorama", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]));
+			
+		}while(opcaoMenu != "5" || opcaoMenu == "-1");
 	}
 	
-	private static void EscreverMenu() {
-		System.out.println("Bem vindo!!");
-		System.out.println("Escolha uma opção para acessar o menu: ");
-		System.out.println("	- 1 'Cadastrar Veículo'");
-		System.out.println("	- 0 'Sair'");
-		
-	}
-	
-	private static void MenuEscolha(String opcaoMenu) {
+	private static void MenuEscolha(String opcaoMenu, Veiculo novoVeiculo) {
 		switch(opcaoMenu) {
-		case "Cadastrar Veículo":
-			Scanner teclado = new Scanner(System.in);
-			System.out.println("Digite a marca do veículo que deseja cadastrar: ");
-			String marcaVeiculo = teclado.nextLine();
-			System.out.println("Digite cor do veículo que deseja cadastrar: ");
-			String cor = teclado.nextLine();
-			System.out.println("Digite o modelo do veículo que deseja cadastrar: ");
-			String modeloVeiculo = teclado.nextLine();
-			System.out.println("Digite o ano de fabricação do veículo que deseja cadastrar: ");
-			int anoFab = teclado.nextInt();
-			System.out.println("Digite o ano do modelo do veículo que deseja cadastrar: ");
-			int anoModelo = teclado.nextInt();
-			System.out.println("Digite a placa do veículo que deseja cadastrar: ");
-			int placa = teclado.nextInt();
-			System.out.println("Digite a capacidade do tanque do veículo que deseja cadastrar: ");
-			int capacidade = teclado.nextInt();
-			System.out.println("Digite o renavam do veículo que deseja cadastrar: ");
-			int renavam = teclado.nextInt();
-			System.out.println("Digite o tipo de combustível do veículo que deseja cadastrar: ");
-			int tipoCombust = teclado.nextInt();
-			System.out.println("Digite o último tipo de combustível abastecido do veículo que deseja cadastrar: ");
-			int combustAtual = teclado.nextInt();			
-			System.out.println("Digite a motorizacao do veículo que deseja cadastrar: ");
-			double motorizacao = teclado.nextDouble();
-			
-			
-			Veiculo Ana = new Veiculo(marcaVeiculo, cor, modeloVeiculo, anoFab, anoModelo, placa, capacidade, renavam, tipoCombust, combustAtual, motorizacao);
+		case "0":
+			novoVeiculo = Veiculo.Init();
 			break;
-		case "":
+		case "1":
+			
 			break;
 		}
 		
--- Carrorama/src/proprietario/Veiculo.java
+++ Carrorama/src/proprietario/Veiculo.java
@@ -1,7 +1,11 @@
 package proprietario;
 
-import despesas.*;
+import despesas.Despesas;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 
 public class Veiculo {
 
@@ -10,27 +14,56 @@ public class Veiculo {
 	private String modelo;
 	private int anoDeFabricacao;
 	private int anoDoModelo;
-	private int placa;
+	private String placa;
 	private int capacidade;
 	private int renavam;
 	private int tipoDeCombustivel;
 	private int combustivelAtual;
 	private double motorizacao;
-	private ArrayList <Despesas> veiculo = new ArrayList();
+	private ArrayList <Despesas> despesasVeiculo = new ArrayList();
+	
+	public static Veiculo Init() {
+		try {
+			
+			String modeloVeiculo = JOptionPane.showInputDialog("Modelo:");
+			String corVeiculo = JOptionPane.showInputDialog("Cor do veículo:");
+			String marca = JOptionPane.showInputDialog("Marca:");
+			String anoDeFabricacao = JOptionPane.showInputDialog("Ano de Fabricação:");
+			String anoDoModelo = JOptionPane.showInputDialog("Ano do Modelo:");
+			String placa = JOptionPane.showInputDialog("Placa:");
+			String capacidade = JOptionPane.showInputDialog("Capacidade do Tanque:");
+			String renavam = JOptionPane.showInputDialog("Renavam:");
+			String[] opcoesCombustivel = {"1 - Gasolina", "2 - Álcool", "3 - Diesel", "4 - Flex"};
+			String tipoCombustivel = Integer.toString(JOptionPane.showOptionDialog(null,"Tipo de Combustível:", "Carrorama", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoesCombustivel, opcoesCombustivel[0]));;
+			String combustivelAtual = JOptionPane.showInputDialog("Quantidade de combustível atual:");
+			String motorizacao = JOptionPane.showInputDialog("Motorização do veículo:");
+			Veiculo novoVeiculo = new Veiculo(marca, corVeiculo, modeloVeiculo, anoDeFabricacao, anoDoModelo, placa, capacidade, renavam, tipoCombustivel, combustivelAtual,motorizacao);
 
-	public Veiculo(String marca, String cor, String modelo, int anoDeFabricacao, int anoDoModelo, int placa, int capacidade, int renavam, int tipoDeCombustivel, int combustivelAtual, double motorizacao) {
+			return novoVeiculo;
+		}catch(Exception e){
+			return null;
+		}
+		
+		
+	}
+
+	public Veiculo(String marca, String cor, String modelo, String anoDeFabricacao, String anoDoModelo, String placa, String capacidadeTanque, String renavam, String tipoDeCombustivel, String combustivelAtual, String motorizacao) {
 		
 		this.marca = marca;
 		this.cor = cor;
 		this.modelo = modelo;
-		this.anoDeFabricacao = anoDeFabricacao;
-		this.anoDoModelo = anoDoModelo;
+		this.anoDeFabricacao = Integer.parseInt(anoDeFabricacao);
+		this.anoDoModelo = Integer.parseInt(anoDoModelo);
 		this.placa = placa;
-		this.capacidade = capacidade;
-		this.renavam = renavam;
-		this.tipoDeCombustivel = tipoDeCombustivel;
-		this.combustivelAtual = combustivelAtual;		
-		this.motorizacao = motorizacao;
+		this.capacidade = Integer.parseInt(capacidadeTanque);
+		this.renavam = Integer.parseInt(renavam);
+		this.tipoDeCombustivel = Integer.parseInt(tipoDeCombustivel);
+		this.combustivelAtual = Integer.parseInt(combustivelAtual);		
+		this.motorizacao = Integer.parseInt(motorizacao);
+	}
+	
+	public Veiculo() {
+		
 	}
 
 	public String getMarca() {
@@ -53,7 +86,7 @@ public class Veiculo {
 		return anoDoModelo;
 	}
 
-	public int getPlaca() {
+	public String getPlaca() {
 		return placa;
 	}
 
