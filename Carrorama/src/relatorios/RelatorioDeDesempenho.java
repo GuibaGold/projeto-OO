3745ae4184a474f808ded8ceeec418f4e9bea6bc
Ailamar Alves <ailamar.alvesg@gmail.com>
2019-07-10 13:43:11 -0300

    Add relatorioDesempenho

--- Carrorama/src/relatorios/RelatorioDeDesempenho.java
+++ Carrorama/src/relatorios/RelatorioDeDesempenho.java
@@ -2,14 +2,38 @@ package relatorios;
 
 import java.util.List;
 
-import despesas.Despesas;
+import despesas.*;
+import proprietario.*;
 
 public class RelatorioDeDesempenho {
 	
-	private List<Despesas> despesa;
+	static double kmlit, kmanterior;
 	
-	public RelatorioDeDesempenho() {
-		
+	
+	public static void RelDesempenho(List<Veiculo> veiculos) {
+		try {
+			int aux = 0;
+			for(Veiculo veiculo : veiculos) {
+				System.out.println("Veiculo de placa "+ veiculo.getPlaca());
+				for(Despesas despesa : veiculo.getListDespesas()) {
+					if(despesa instanceof Abastecimento) {
+						if(aux==0) {
+							if(((Abastecimento) despesa).isTanqueCheio()==true) {
+								aux=1;
+								kmanterior = ((Abastecimento) despesa).getquilometragem();
+							}
+						} else {
+							if(((Abastecimento) despesa).isTanqueCheio()==true) {
+								kmlit = (((Abastecimento) despesa).getquilometragem()-kmanterior)/((despesa.getValor())/((Abastecimento) despesa).getValorDoLitro());    
+							} 
+						}
+					}
+				}
+			}
+		} catch(Exception e) {
+			System.out.println("Não há despesa ou veículo");
+			e.printStackTrace();
+		}
 	}
 
 }
